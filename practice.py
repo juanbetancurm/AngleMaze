0557b847aca330d40f14ff7e66c064f472952418
juanjbetancur <juan.jbetancur852@gmail.com>
2025-06-14 12:56:54 -0500

    Game Working

---
+++ practice.py
@@ -0,0 +1,6 @@
+various = ('Juan', 'Pérez', 35, 'Colombia', 'Español')
+
+nombre, apellido, edad, pais, idioma = various
+
+
+print(f'Este es {nombre} {apellido} de {edad} años que nos visita desde {pais} y nos habla en {idioma}')
